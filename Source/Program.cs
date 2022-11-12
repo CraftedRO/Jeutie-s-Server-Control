8298b1c4c299421675311334501d4cd38dd4a24e
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2022-11-12 12:40:13 +0200

    updated binary

--- Source/Program.cs
+++ Source/Program.cs
@@ -17,7 +17,7 @@ namespace JeutieControl
                 {
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
-                    Application.Run(new Form1());
+                    Application.Run((Form) new Form1());
                     mtex.ReleaseMutex();
                 }
                 else
